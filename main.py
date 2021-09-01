3329ed2e143d16c5c719f6bebac56697a36a9c40
Shine Barbhuiya <shinealom@gmail.com>
2021-09-01 18:34:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-dev-shm-usage')
 chrome_options.add_argument("--start-maximized")
-chrome_options.add_argument('headless')
+chrome_options.add_argument('--headless')
 
 
 
